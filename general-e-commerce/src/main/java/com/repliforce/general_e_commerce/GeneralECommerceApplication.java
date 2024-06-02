d69e8bf36f01bb2eaa4e9e546031903c34265c1f
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-06-01 22:09:14 -0300

    Starting e-commerce fullstack project

---
+++ general-e-commerce/src/main/java/com/repliforce/general_e_commerce/GeneralECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.repliforce.general_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralECommerceApplication.class, args);
+	}
+
+}
---
+++ general-e-commerce/src/test/java/com/repliforce/general_e_commerce/GeneralECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repliforce.general_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
