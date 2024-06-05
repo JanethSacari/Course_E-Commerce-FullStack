535fca42578947661b4695f20334fb6eed144532
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-06-04 23:20:02 -0300

    E-commerce-FullStack: Basic entity product created

---
+++ general-e-commerce/src/main/java/com/repliforce/general_e_commerce/entities/Product.java
@@ -0,0 +1,25 @@
+package com.repliforce.general_e_commerce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+
+    private Long id;
+    private String code;
+    private String name;
+    private String description;
+    private BigDecimal unitPrice;
+    private String imageUrl;
+    private boolean active;
+    private int unitsInStock;
+    private Date dateCreated;
+    private Date lastUpdated;
+}
